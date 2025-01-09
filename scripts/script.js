1b9803b4f974ed4480891df0e0eb5ca5cb8b4d73
kim <Kimvz19@users.noreply.github.com>
2025-01-09 18:05:40 +0100

    adding 1ste surface plane optie

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,31 @@
 // JavaScript Document
 console.log("hi");
-console.log("it works")
+console.log("it works");
 
-//Variabelen aanmaken
-let buttons = document.querySelectorAll('button');
+// variabelen
+let darkModeButton = document.querySelector('.pinkModeButton');
 
-let soundButton = buttons[0];
-let darkModeButton = buttons[1];
-let kylieBackgroundButton = buttons[2];  
+// Functies
+
+//functie roze kleur te veranderen
+darkModeButton.addEventListener('click', () => {
+    // Controleer of de dark mode al is geactiveerd
+    let isPinkMode = document.body.classList.toggle('pink-mode');
+    
+    // :root aanpasssen
+    if (isPinkMode) {
+        // Verander de variabelen naar pink-mode
+        document.documentElement.style.setProperty('--color-pink-100', '#FFC1E3');
+        document.documentElement.style.setProperty('--color-pink-200', '#FF8CB6');
+        document.documentElement.style.setProperty('--color-pink-300', '#FF70A6');
+        document.documentElement.style.setProperty('--color-pink-500', '#FF007F');
+    } else {
+        // Zet de standaardkleuren terug
+        document.documentElement.style.setProperty('--color-pink-100', '#f8f1f4');
+        document.documentElement.style.setProperty('--color-pink-200', '#e7dae2');
+        document.documentElement.style.setProperty('--color-pink-300', '#efd7e5');
+        document.documentElement.style.setProperty('--color-pink-500', '#b3848f');
+    }
+});
 
 
-// // Functie om dark mode in te schakelen
-// darkModeButton.addEventListener('click', () => {
-//     document.body.classList.toggle('dark-mode'); // Toggle de dark mode klasse
-// });
-
-// // Functie om de Kylie achtergrond in te schakelen
-// kylieBackgroundButton.addEventListener('click', () => {
-//     document.body.classList.toggle('kylie-background'); // Toggle de Kylie achtergrond
-// });
