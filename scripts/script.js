b163a0987b1d643e8bf7b1c80c554ae12082a863
kim <Kimvz19@users.noreply.github.com>
2025-01-09 17:36:53 +0100

    adding option voor darkmode

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,21 @@
 // JavaScript Document
 console.log("hi");
 console.log("it works")
+
+//Variabelen aanmaken
+let buttons = document.querySelectorAll('button');
+
+let soundButton = buttons[0];
+let darkModeButton = buttons[1];
+let kylieBackgroundButton = buttons[2];  
+
+
+// // Functie om dark mode in te schakelen
+// darkModeButton.addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode'); // Toggle de dark mode klasse
+// });
+
+// // Functie om de Kylie achtergrond in te schakelen
+// kylieBackgroundButton.addEventListener('click', () => {
+//     document.body.classList.toggle('kylie-background'); // Toggle de Kylie achtergrond
+// });
