f82e1cd3ecd864904d70b50f20d41cb9807b8663
kim <Kimvz19@users.noreply.github.com>
2025-01-16 20:12:26 +0100

    js opschonen

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// JavaScript Document
+//console check
 console.log("hi");
 console.log("it works");
 
@@ -11,22 +11,23 @@ let videoElements = document.querySelectorAll('video');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 let hiddenMenu = document.querySelector('.hidden-menu');
 let closeMenu = document.querySelector('.close-menu'); 
+let header = document.querySelector('header');
+let buttons = document.querySelectorAll('button');
 
 // Functies
 
-// Openen 
+
+// hamburger menu icon openen
 hamburgerMenu.addEventListener("click", () => {
     hiddenMenu.classList.toggle("open");
 });
 
-// Sluiten van het menu via de kruis-knop
+// Sluiten kruisje hamburger
 closeMenu.addEventListener("click", () => {
     hiddenMenu.classList.remove("open");
 });
 
-
 //video advent calendar
-
 videoElements.forEach(function(videoElement) {
   videoElement.addEventListener('click', function() {
 
@@ -39,22 +40,20 @@ videoElements.forEach(function(videoElement) {
 });
 
 
-
-
-//functie roze kleur te veranderen
+//functie roze kleur te veranderen (dark mode version pink)
 darkModeButton.addEventListener('click', () => {
-    // Controleer of de dark mode al is geactiveerd
+    // pink mode
     let isPinkMode = document.body.classList.toggle('pink-mode');
     
     // :root aanpasssen
     if (isPinkMode) {
-        // Verander de variabelen naar pink-mode
+        // variablen veranderen 
         document.documentElement.style.setProperty('--color-pink-100', '#FFC1E3');
         document.documentElement.style.setProperty('--color-pink-200', '#FF8CB6');
         document.documentElement.style.setProperty('--color-pink-300', '#FF70A6');
         document.documentElement.style.setProperty('--color-pink-500', '#FF007F');
     } else {
-        // Zet de standaardkleuren terug
+        // standaardkleuren
         document.documentElement.style.setProperty('--color-pink-100', '#f8f1f4');
         document.documentElement.style.setProperty('--color-pink-200', '#e7dae2');
         document.documentElement.style.setProperty('--color-pink-300', '#efd7e5');
@@ -62,29 +61,27 @@ darkModeButton.addEventListener('click', () => {
     }
 });
 
+
+ // kylie sound
 kylieSound.addEventListener('click', ()=> {
     let audio = new Audio('audio/kylie_rise_and_shine.mp3');
     audio.play();
 });
 
-
+ // kylie logo buttons & background
 kingKy.addEventListener('click', () => {
-    //variabelen
-    let header = document.querySelector('header');
-    let buttons = document.querySelectorAll('button');
-
     if (isKylieBackgroundActive) {
-        // Verwijder de achtergrondafbeelding van de header en buttons
+        // Verwijder de achtergrondafbeelding van header & buttons
         header.style.backgroundImage = "";
         buttons.forEach(button => {
             button.style.backgroundImage = "";
-            button.style.color = ""; // Reset tekstkleur naar standaard
+            button.style.color = ""; // oude kleuren terug
         });
         isKylieBackgroundActive = false;
 
         
     } else {
-        // Voeg de achtergrondafbeelding toe aan de header en buttons
+        // kylie background toevoegen
         header.style.backgroundImage = "url('images/extra-features/new-kylie-old-logo-background.png')";
         header.style.backgroundSize = "cover";
         header.style.backgroundPosition = "center";
@@ -93,7 +90,7 @@ kingKy.addEventListener('click', () => {
             button.style.backgroundImage = "url('images/extra-features/new-kylie-old-logo-background.png')";
             button.style.backgroundSize = "cover";
             button.style.backgroundPosition = "center";
-            button.style.color = "black"; // Zet de tekstkleur van de knop op zwart
+            button.style.color = "black";
         });
         isKylieBackgroundActive = true;
     }
@@ -108,17 +105,14 @@ function handleSubmit() {
     let dialogMessage = document.querySelector('.dialogMessage');
     let email = emailInput.value.trim();
 
-    // Controleer of het emailadres geldig is volgens de HTML5-validatie
+    // controle email
     if (!emailInput.checkValidity()) {
         dialogMessage.textContent = 'Please enter a valid email address';
     } else {
         dialogMessage.textContent = `Thank you for signing up!`;
 
-        // Maak het invoerveld leeg nadat het e-mailadres is goedgekeurd
-        emailInput.value = ''; // Reset het invoerveld
+        emailInput.value = '';
     }
-
-
     emailDialog.showModal();
 }
 
